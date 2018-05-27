7abc3f1927443ba7f2c2d14950e16061d3078938
Megan <musicalmacdonald@gmail.com>
2018-05-27 13:15:18 -0600

    .sh file, acquires latest code from master for testing

--- pi_client/accelerometer_post_test.py
+++ pi_client/accelerometer_post_test.py
@@ -14,9 +14,6 @@ class test_accelerometer_post(unittest.TestCase):
         l = ap.getServerList()
         self.assertTrue(len(l)>0
 
-def get_ServerList():
-    ServerList = (https://megan-pi-iot.cfapps.io/index.html, https://katie-pi-iot.cfapps.io/index.html, https://david-pi-iot.cfapps.io/index.html, https://jpf-pi-iot.cfapps.io/index.html, https://shane-pi-iot.cfapps.io/index.html)
-    return ServerList
 
 if __name__ == '__main__':
 unittest.main()
