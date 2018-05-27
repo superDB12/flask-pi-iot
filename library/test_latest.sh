3d4d1779910706fe7b75d69bf452437a161b5bf0
Megan <musicalmacdonald@gmail.com>
2018-05-27 13:08:56 -0600

    .sh file, acquires latest code from master for testing

---
+++ library/test_latest.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo *** getting lastest from master ****
+echo pi_accelerometer_IOT_client_test
+curl -OL https://raw.githubusercontent.com/musicalmacdonald/flask-pi-iot/master/pi_client/accelerometer_post_test.py > accelerometer_post_test.py
+echo pi_accelerometer_IOT_client
+curl -OL https://raw.githubusercontent.com/musicalmacdonald/flask-pi-iot/master/pi_client/accelerometer_post.py > accelerometer_post.py
+python accelerometer_post_test.py -v
